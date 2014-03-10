093b30058a5a9bbbc08eb8e17329e8cfc8e34536
Lawrence Murray <lawrence.murray@csiro.au>
2014-03-10 20:46:57 +0800

    Added plot_metric().

---
+++ oct/plot_metric.m
@@ -0,0 +1,55 @@
+function plot_metric(metric)
+    if nargin < 1
+        metric = 1;
+    end
+    
+    P = {};
+    y = {};
+    filters = {'Bootstrap'; 'Bridge'};
+    for i = 1:length(filters)
+        y{i} = [];
+        P{i} = [];
+        rep = 0;
+        file = sprintf('results/test_%s-%d.nc', tolower(filters{i}), rep);
+        while exist(file, 'file')
+            nc = netcdf(file, 'r');
+            L = nc{'loglikelihood'}(:,:)';
+            T = nc{'time'}(:,:)';
+        
+            if metric == 1
+                tmp = ess(L)'./mean(T)';
+            elseif metric == 2
+                tmp = car(L)'./mean(T)';
+            elseif metric == 3
+                tmp = mean(L)';
+            elseif metric == 4
+                tmp = -std(L)'./mean(T)';
+            end
+            y{i} = [ y{i}; tmp ];
+            P{i} = [ P{i}; nc{'P'}(:) ];
+            ncclose(nc);
+            
+            rep = rep + 1;
+            file = sprintf('results/test_%s-%d.nc', tolower(filters{i}), rep);
+        end
+    end
+
+    for i=1:rows(y{1})
+        if y{1}(i) > y{2}(i)
+            col = watercolour(1);
+        else
+            col = watercolour(2);
+        end
+        h = loglog(y{1}(i), y{2}(i), 'o', 'color', col, 'markersize', ...
+                   sqrt(P{1}(i)/pi), 'linewidth', 3);
+        hold on;
+    end
+
+    %axis('tight');
+    ax = axis();
+    mn = min([ax(1), ax(3)]);
+    mx = max([ax(2), ax(4)]);
+    loglog([mn mx], [mn mx], '--k');
+    axis([mn mx mn mx]);
+    axis('square');
+end
