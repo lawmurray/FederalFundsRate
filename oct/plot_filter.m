b451714681ab9be7dc6281632b2e04754ff5fe62
Lawrence Murray <lawrence.murray@csiro.au>
2014-03-14 17:59:49 +0930

    Updated filter plot and number of particles.

--- oct/plot_filter.m
+++ oct/plot_filter.m
@@ -1,31 +1,32 @@
 function plot_filter
   ax = [0 1 -0.05 0.2];
   ps = [];
-  filters = {'Kalman'; 'Bootstrap'; 'Bridge'};
+  filters = {'Bootstrap'; 'Bridge'};
 
   for i = 1:length(filters);
       file = sprintf('results/filter_%s.nc', tolower(filters{i}));
       subplot(1, length(filters), i);
-      if strcmp(filters{i}, 'Kalman')
-          bi_plot_quantiles(file, 'x', [], [], [], 3);
-      else
-          bi_plot_paths(file, 'x', [], ps, [], i - 1);
-      end
-      hold on;
       
-      if !strcmp(filters{i}, 'Kalman')
-          nc = netcdf(file, 'r');
-          t = nc{'time'}(:);
-          A = nc{'ancestor'}(:,:)';
-          for j = 1:columns(A)
-              if sum(A(:,j) != [0:(rows(A)-1)]') > 0
-                  % resampling occurred at this point
-                  line([t(j) t(j)], [ax(3) ax(4)], 'color', 'k');
-                  %t
-              end
+      nc = netcdf(file, 'r');
+      t = nc{'time'}(:);
+      A = nc{'ancestor'}(:,:)';
+      from = 1;
+      to = 0;
+      for j = 1:columns(A)
+          if sum(A(:,j) != [0:(rows(A)-1)]') > 0
+              % resampling occurred at this point
+              patch([t(to) t(to + 1) t(to + 1) t(to)], [ax(3) ax(3) ax(4) ...
+                                  ax(4)], 'facecolor', [0.9 0.9 0.9], ...
+                    'facealpha', 0.5);
+              hold on;
+              bi_plot_paths(file, 'x', [], ps, [from:to], i);
+              from = j;
           end
-          ncclose(nc);
+          to = j;
       end
+      bi_plot_paths(file, 'x', [], ps, [from:to], i);
+      hold on;
+      ncclose(nc);
       
       bi_plot_paths('data/obs_set.nc', 'y');
       hold off;
