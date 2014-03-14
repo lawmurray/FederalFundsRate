63b2ca855ea61b4c583e36a96aff3c96e89592c0
Lawrence Murray <lawrence.murray@csiro.au>
2014-03-14 19:58:45 +1100

    Removed Kalman filter run script.

--- qsub_run_kalman.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#PBS -l walltime=00:30:00,nodes=1:ppn=1,vmem=8gb -j oe
-
-source $HOME/init.sh
-cd $PBS_O_WORKDIR
-
-libbi filter @config.conf @filter.conf --filter kalman --output-file results/filter_kalman.nc
-libbi sample @config.conf @posterior.conf --filter kalman --output-file results/posterior_kalman.nc
