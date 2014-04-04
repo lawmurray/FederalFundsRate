ee68e0488e375d77e903212de758254a0a96f0fe
Lawrence Murray <lawrence.murray@csiro.au>
2014-04-04 19:55:01 +1100

    Added both bootstrap and bridge filters to run.sh. Updated required execution time in qsub_run.sh.

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-libbi filter @config.conf @filter.conf
+libbi filter @config.conf @filter.conf --filter bootstrap --output-file results/filter_bootstrap.nc
+libbi filter @config.conf @filter.conf --filter bridge --output-file results/filter_bridge.nc
 libbi sample @config.conf @posterior.conf
